bf7e84ae758cde6748fad8fcf034e36a32ee9879
Reto Lehnherr <reto.lehnherr@meteotest.ch>
2014-06-18 12:08:10 +0200

    Added WTFException.php

---
+++ WTFException.php
@@ -0,0 +1,14 @@
+class WTFException extends Exception
+{
+
+    public function __construct($message = null, $code = null)
+    {
+        if (! $message) {
+            $message = "WTF!?";
+        } else {
+            $message = "WTF!? " . $message;
+        }
+        
+        parent::__construct($message, $code);
+    }
+}
